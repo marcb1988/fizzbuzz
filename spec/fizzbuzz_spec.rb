a1c00a9a248be5b7cb91a8a2615e947cf5799670
Marc Blenkinsop <marcblenky@hotmail.com>
2019-10-07 16:58:06 +0100

    completed code and tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,3 +5,21 @@ describe 'fizzbuzz' do
       expect(fizzbuzz(3)).to eq 'fizz'
     end
   end
+
+describe 'fizzbuzz' do
+    it 'returns "buzz" when passed 5' do
+        expect(fizzbuzz(5)).to eq 'buzz'
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'returns "fizzbuzz" when passed 15' do
+        expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'return "4" if passed 4' do
+        expect(fizzbuzz(4)).to eq 4
+    end
+end
